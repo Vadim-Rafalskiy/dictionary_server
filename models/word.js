3a17d3df9a4e200b67e9e5c5817adbaea783a1b7
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-09-10 01:55:49 +0200

    update mongoose schema for wordSchema

--- models/word.js
+++ models/word.js
@@ -1,11 +1,29 @@
 const { Schema, model } = require('mongoose');
 const Joi = require('joi');
 
-const wordSchema = new Schema({
-    pl: String,
-    uk: String,
-    serf: String,
-    partOfSpeech: String,
+const wordSchema = new Schema(
+    {
+        pl: {
+            type: String,
+            required: true,
+        },
+        uk: {
+            type: String,
+            required: true,
+        },
+        serf: {
+            type: String,
+            enum: ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'],
+            required: true,
+        },
+        partOfSpeech: {
+            type: String,
+            required: true,
+        },
+    },
+    { versionKey: false, timestamps: true }
+);
+
 const addJoiSchema = Joi.object({
     pl: Joi.string().required(),
     uk: Joi.string().required(),
