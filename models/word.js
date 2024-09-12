12ee7a5e20f961e0af1bdb6e3bd2bcdcca9d5988
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-09-12 20:00:30 +0200

    add to mongoose-schema required error message

--- models/word.js
+++ models/word.js
@@ -1,28 +1,33 @@
 const { Schema, model } = require('mongoose');
 const Joi = require('joi');
+const { handleMongooseError } = require('../utils');
+
+const sertifikatesList = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
 
 const wordSchema = new Schema(
     {
         pl: {
             type: String,
-            required: true,
+            required: [true, '"pl" must exist and must be a string'],
         },
         uk: {
             type: String,
-            required: true,
+            required: [true, '"uk" must exist and must be a string'],
         },
         serf: {
             type: String,
-            enum: ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'],
+            enum: sertifikatesList,
             required: true,
         },
         partOfSpeech: {
             type: String,
-            required: true,
+            required: [true, '"partOfSpeech" must exist and must be a string'],
         },
     },
     { versionKey: false, timestamps: true }
 );
+
+wordSchema.post('save', handleMongooseError);
 
 const addJoiSchema = Joi.object({
     pl: Joi.string().required(),
