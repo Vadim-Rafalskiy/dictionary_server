a62dc15510fba53f42b33ceb28c8cd2f0979dff7
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-09-17 20:11:54 +0200

    update auth-routes.js (add ruotes for register and login)

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const ctrl = require('../../controllers/ctrl-auth');
 
 const { validateBody } = require('../../utils');
+const { userJoiSchemas } = require('../../models/user');
 
+router.post('/register', validateBody(userJoiSchemas.register), ctrl.register);
+router.post('/login', validateBody(userJoiSchemas.login), ctrl.login);
 
 module.exports = router;
