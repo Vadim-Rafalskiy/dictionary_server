bb4687f0d514641d7dd227ae08e0e3b63debbbc5
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-09-17 20:10:27 +0200

    create controllers to auth

---
+++ controllers/ctrl-auth.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { ctrlWrapper } = require('../utils');
+require('dotenv').config();
+
+const { User } = require('../models/user');
+
+const { HttpError } = require('../helpers');
+
+const register = async (req, res) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) throw HttpError(409, 'Email already exists');
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({ ...req.body, password: hashPassword });
+    res.status(201).json({
+        name: newUser.name,
+        email: newUser.email,
+    });
+};
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const { SECRET_KEY } = process.env;
+
+    const user = await User.findOne({ email });
+    if (!user) throw HttpError(401, 'Email or password is invalid');
+
+    const isPasswordCompaire = await bcrypt.compare(password, user.password);
+    if (!isPasswordCompaire)
+        throw HttpError(401, 'Email or password is invalid');
+
+    const payload = { id: user._id };
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+
+    res.json({
+        token,
+    });
+};
+
+module.exports = {
+    register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
+};
