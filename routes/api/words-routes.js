c26687ee8867a5a6b0ad3e8fbdd34b595592da7b
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-09-18 00:26:44 +0200

    create middleware authenticate

--- routes/api/words-routes.js
+++ routes/api/words-routes.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const ctrl = require('../../controllers/ctrl-words');
 
 const { validateBody } = require('../../utils');
-const { isValidId } = require('../../middlewares');
+const { isValidId, authenticate } = require('../../middlewares');
 
 const { addWordJoiSchema } = require('../../models/word');
 
-router.get('/', ctrl.getAllWords);
+router.get('/', authenticate, ctrl.getAllWords);
 
-router.get('/:id', isValidId, ctrl.getWordById);
+router.get('/:id', authenticate, isValidId, ctrl.getWordById);
 
-router.post('/', validateBody(addWordJoiSchema), ctrl.addWord);
+router.post('/', authenticate, validateBody(addWordJoiSchema), ctrl.addWord);
 
 router.put(
     '/:id',
