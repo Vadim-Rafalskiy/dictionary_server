d6af942132f5df0fc1789b4cc2e5cb42cc6b2ae5
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-09-18 11:09:18 +0200

    create route "/current"

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { authenticate } = require('../../middlewares');
+
 const ctrl = require('../../controllers/ctrl-auth');
 
 const { validateBody } = require('../../utils');
@@ -8,5 +10,6 @@ const { userJoiSchemas } = require('../../models/user');
 
 router.post('/register', validateBody(userJoiSchemas.register), ctrl.register);
 router.post('/login', validateBody(userJoiSchemas.login), ctrl.login);
+router.get('/current', authenticate, ctrl.getCurrent);
 
 module.exports = router;
